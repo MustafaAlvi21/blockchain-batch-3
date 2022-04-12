ad5be617753a533bee2421147335934cdde4ef6a
MustafaAlvi21 <mustafaalvi21@gmail.com>
2022-04-12 05:04:20 -0700

    class #15&16

--- class #14/server.js
+++ class #14/server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
+const fs = require("fs")
 
-const PORT = 4001
+const PORT = 4000
 var JSONDATA = [
     {
         name: "Ayesha", 
@@ -24,11 +25,55 @@ var JSONDATA = [
         image: "https://reqres.in/img/faces/4-image.jpg"
     }
 ]
+var textData = fs.readFileSync('readme.txt', "utf-8", (err, data) => {
+    if (err) throw err;
+    return data
+});
+console.log("-----------------------------------------");
+console.log(textData);
+console.log("-----------------------------------------");
+
+data = `.p{ font-size: 25px}`
+fs.writeFileSync(`./public/css/metadata.css`, data);
+
+const bodyParser = require('body-parser');
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express.static("public"))
 
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
     // route ki logic
-    res.send(JSONDATA) 
+    var textData = fs.readFileSync(`./public/css/metadata.css`, "utf-8", (err, data) => {
+        if (err) throw err;
+        return data
+    });
+
+    data = ` ${textData}
+
+    h1{ 
+        font-size: 25px;
+        color: red;
+    }
+    body{
+        background-color: gray;
+    }
+    `
+    fs.writeFileSync(`./public/css/metadata.css`, data);
+    
+    
+    res.send("file updated")
+})
+
+// delete file
+app.get("/delete", (req, res) => {
+    fs.unlinkSync(`./public/css/metadata.css`);
+    res.send("File deleted")
 })
 
 app.get("/profile/:name", (req, res) => {
@@ -40,6 +85,41 @@ app.get("/profile/:name", (req, res) => {
     })
     res.send(data) 
 })
+
+app.post("/home", (req, res) => {
+    console.log(req.body);
+    res.json(req.body)
+})
+
+
+// params example
+app.get("/data/:name/:age/:id", (req, res) => {
+    console.log(req.params);
+    res.send(req.params)
+})
+
+
+// query example
+app.get("/my-query", (req, res) => {
+    console.log(req.query);
+    res.send(req.query)
+})
+
+
+// post example
+app.post("/postdata", (req, res) => {
+
+})
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log("server is running");
